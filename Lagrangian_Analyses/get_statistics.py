ae6433abb2fe646e012d891e9b1b22e28961381e
MireyaMMMO <mireya.mmor@gmail.com>
2023-12-29 09:28:38 +1300

    Adding second notebook, getting stats (PDF, Connectivity Matrix, and stranding associated data

--- Lagrangian_Analyses/get_statistics.py
+++ Lagrangian_Analyses/get_statistics.py
@@ -8,9 +8,10 @@ from scipy.interpolate import interp2d
 from .utils import *
 import pickle
 import matplotlib.pyplot as plt
+import os
 
-plt.rcParams['font.family'] = 'DeJavu Serif'
-plt.rcParams['font.serif'] = ['Times New Roman']  
+plt.rcParams["font.family"] = "DeJavu Serif"
+plt.rcParams["font.serif"] = ["Times New Roman"]
 
 logging.basicConfig(level=logging.INFO)
 
@@ -19,6 +20,7 @@ class get_statistics(object):
     def __init__(
         self,
         file_list,
+        outdir,
         id=None,
         PDF=[120, 90],
         CM=True,
@@ -29,17 +31,25 @@ class get_statistics(object):
         patch=False,
     ):
         self.file_list = file_list
+        self.outdir = outdir
         self.id = id
         self.PDF = PDF
         self.CM = CM
+        self.CV = CV
         self.stranded = stranded
         self.patch = patch
         self.time_of_interest = time_of_interest
-        self.time_interest_units = time_of_interest_units
+        self.time_of_interest_units = time_of_interest_units
         self.first_PDF = True
         self.first_CM = True
         self.first_stranded = True
         self.logger = logging
+
+    def set_directories(self):
+        """Create output directories."""
+        self.logger.info("--- Creating output directory")
+        if not os.path.isdir(self.outdir):
+            os.makedirs(self.outdir)
 
     def get_requested_time_step_index(self, time):
         delta_time = time[1] - time[0]
@@ -51,7 +61,7 @@ class get_statistics(object):
 
     def get_connectivity_matrix(self, CM, patches):
         temporal_arr = make_nan_array(len(patches), 275000)
-        matrix = make_nan_array(self.number_of_release_locations, len(patches) + 1)
+        matrix = np.zeros((len(self.release_locations), len(patches) + 1))
         for count, patch in enumerate(patches):
             pt1 = matplotlib.path.Path(patch)
             grid = []
@@ -64,11 +74,16 @@ class get_statistics(object):
                 if j < matrix.shape[1] - 1:
                     matrix[i, j] = len(np.where(temporal_arr[j, :] == i)[0])
                 else:
-                    matrix[i, j] = np.nansum(self.number_of_particles - matrix[j, 0:-1])
-        matrix[np.where(matrix == 0)] = np.nan
+                    self.number_of_particles_per_location = (
+                        self.number_of_particles / len(self.release_locations)
+                    )
+                    matrix[i, j] = self.number_of_particles_per_location - np.nansum(
+                        matrix[i, 0:-1]
+                    )
+        # matrix[np.where(matrix == 0)] = np.nan
         return matrix
 
-    def centers(edges):
+    def centers(self, edges):
         return edges[:-1] + np.diff(edges[:2]) / 2
 
     def get_pdf(self, PDF):
@@ -94,7 +109,7 @@ class get_statistics(object):
         lon = lon.where(lon > 0, lon + 360)
         lat = ds.lat.where(ds.lat < 9.0e35, np.nan)
         if isinstance(self.time_of_interest, int):
-            requested_index = self.get_requested_time_step_index(self, ds.time)
+            requested_index = self.get_requested_time_step_index(ds.time)
         elif self.time_of_interest == "last":
             requested_index = -1
         status = ds.status.where(~np.isnan(lon), np.nan)
@@ -107,9 +122,9 @@ class get_statistics(object):
     ):
         CM = make_nan_array(number_of_particles, 3)
         for count in range(number_of_particles):
-            active_particles = status.sel(trajectory=count + 1)
-            active_particles = np.where(active_particles == 0)[0]
-            if len(active_particles) >= requested_index:
+            active_particle = status.sel(trajectory=count + 1)
+            active_particles = np.where(active_particle == 0)[0]
+            if len(active_particles) > requested_index:
                 last_index = active_particles[requested_index]
             else:
                 last_index = active_particles[-1]
@@ -121,33 +136,53 @@ class get_statistics(object):
         return CM
 
     def get_stranded_particles(
-        self, lon, lat, requested_index, status, origin_marker, number_of_particles,
+        self,
+        lon,
+        lat,
+        requested_index,
+        status,
+        origin_marker,
+        number_of_particles,
     ):
         for count in range(number_of_particles):
             particles = status.sel(trajectory=count + 1)
             index_of_release = np.where(particles == 0)[0][0]
             stranded_particles = np.where(particles == 1)[0]
-            if stranded_particles.size>0 and stranded_particles < requested_index:
-                time_of_stranding = lon.sel(trajectory=count + 1).isel(time=stranded_particles).time.data
-                time_of_release = lon.sel(trajectory=count + 1).isel(time=index_of_release).time.data
-                duration_s = (time_of_stranding - time_of_release)*1e-9 / 3600
+            if stranded_particles.size > 0 and stranded_particles < requested_index:
+                time_of_stranding = (
+                    lon.sel(trajectory=count + 1)
+                    .isel(time=stranded_particles)
+                    .time.data
+                )
+                time_of_release = (
+                    lon.sel(trajectory=count + 1).isel(time=index_of_release).time.data
+                )
+                duration_s = (time_of_stranding - time_of_release) * 1e-9 / 3600
                 lon_s = lon.sel(trajectory=count + 1).isel(time=stranded_particles).data
                 lat_s = lat.sel(trajectory=count + 1).isel(time=stranded_particles).data
-                om_s = origin_marker.sel(trajectory=count + 1).isel(time=stranded_particles).data
+                om_s = (
+                    origin_marker.sel(trajectory=count + 1)
+                    .isel(time=stranded_particles)
+                    .data
+                )
                 if self.first_stranded:
                     self.duration_before_stranding = duration_s
                     self.lon_stranding = lon_s
                     self.lat_stranding = lat_s
-                    self.om_stranding = om_s 
-                    self.first_stranded=False
+                    self.om_stranding = om_s
+                    self.first_stranded = False
                 else:
-                    self.duration_before_stranding = np.hstack((self.duration_before_stranding, duration_s))
+                    self.duration_before_stranding = np.hstack(
+                        (self.duration_before_stranding, duration_s)
+                    )
                     self.lon_stranding = np.hstack((self.lon_stranding, lon_s))
                     self.lat_stranding = np.hstack((self.lat_stranding, lat_s))
                     self.om_stranding = np.hstack((self.om_stranding, om_s))
-        
+
     def run(self):
-        for file in self.filelist:
+        self.set_directories()
+        for file in self.file_list:
+            print(f"--- Analysing {file}")
             ds = xr.open_dataset(file)
             (
                 lon,
@@ -157,7 +192,7 @@ class get_statistics(object):
                 origin_marker,
                 self.number_of_particles,
             ) = self.get_data_from_file(ds)
-            self.number_of_release_locations = np.unique(origin_marker)[
+            self.release_locations = np.unique(origin_marker)[
                 ~np.isnan(np.unique(origin_marker))
             ]  # make sure there are no NaNs
             CM = self.get_lon_lat_and_origin(
@@ -187,11 +222,18 @@ class get_statistics(object):
                 else:
                     Matrix = np.vstack((Matrix, np.expand_dims(matrix, 0)))
             if self.stranded:
-                self.get_stranded_particles(lon, lat, requested_index, status, origin_marker, self.number_of_particles)
-        
+                self.get_stranded_particles(
+                    lon,
+                    lat,
+                    requested_index,
+                    status,
+                    origin_marker,
+                    self.number_of_particles,
+                )
+        print(f"--- Calculating the average of desired variables")
         if self.PDF:
             print(
-                "Saving PDF associated parameters: normalised PDF (n_PDF), and bin edges (PDF_xedges,)"
+                f"--- Saving PDF associated parameters as {self.outdir}PDF_{self.id}.p \n - normalised PDF (n_PDF) \n - bin edges (xedges, yedges)"
             )
             self.norm_pdf, self.xedges, self.yedges = self.get_pdf(PDF)
             pickle.dump(
@@ -203,9 +245,11 @@ class get_statistics(object):
                 open(f"{self.outdir}/PDF_{self.id}.p", "wb"),
             )
         if self.CM:
+            print(
+                f"--- Saving Connectivity Matrix associated parameters as {self.outdir}/CM_{self.id}.p \n - normalised CM"
+            )
             self.avg_CM = np.nanmean(Matrix, axis=0)[::-1]
-            self.norm_CM = self.avg_CM / np.nansum(self.avg_CM, ax=1)
-            self.norm_CM.dump(f"{self.outdir}/normalised_CM_{self.id}")
+            self.norm_CM = np.divide(self.avg_CM.T, np.nansum(self.avg_CM, axis=1).T).T
             pickle.dump(
                 [
                     self.norm_CM,
@@ -213,7 +257,11 @@ class get_statistics(object):
                 open(f"{self.outdir}/CM_{self.id}.p", "wb"),
             )
         if self.CV:
+            print(
+                "--- Adding Connectivity Matrix associated parameters to {self.outdir}/CM_{self.id}.p \n - CV"
+            )
             std_CM = np.nanstd(Matrix, axis=0)[::-1]
+            std_CM[np.where(std_CM == 0)] = np.nan
             self.CV = std_CM / self.avg_CM
             pickle.dump(
                 [
@@ -223,9 +271,16 @@ class get_statistics(object):
                 open(f"{self.outdir}/CM_{self.id}.p", "wb"),
             )
         if self.stranded:
+            print(
+                f"--- Saving stranded particles data associated parameters as {self.outdir}/Stranded_particles_data_{self.id}.p \n - avg_duration \n - norm_avg_particles_stranded \n - duration before stranding \n - stranding coordinates(lon, lat) \n - origin marker"
+            )
             avg_duration = np.mean(self.duration_before_stranding)
-            avg_particles_stranded = len(self.duration_before_stranding)/len(self.file_list)
-            norm_avg_particles_stranded = np.round(avg_particles_stranded/self.number_of_particles,3)
+            avg_particles_stranded = len(self.duration_before_stranding) / len(
+                self.file_list
+            )
+            norm_avg_particles_stranded = np.round(
+                avg_particles_stranded / self.number_of_particles, 3
+            )
             pickle.dump(
                 [
                     avg_duration,
