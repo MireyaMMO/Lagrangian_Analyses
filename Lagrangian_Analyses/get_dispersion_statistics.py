f102fcd456ce85e897fc240768c4bbdc3ba335db
MireyaMMMO <mireya.mmor@gmail.com>
2023-12-31 19:07:46 +1300

    Final modification and adding notebook 04

--- Lagrangian_Analyses/get_dispersion_statistics.py
+++ Lagrangian_Analyses/get_dispersion_statistics.py
@@ -23,17 +23,18 @@ class get_dispersion_statistics(object):
             path of the directory where to save the output
         id: str
             Id to identify the experiment
-        predominant_direction: str
-            Predominant direction to define the positive and negative values relative to the release location. Default: 'horizontal'
-        time_of_interest: int
-            Time at which the user wants to calculate all this parameters.
-        time_of_interest_units: str
-            Units of the time of interest defined previously. i.e. 'D' for days, 'H' for hours
+        AD: boolean
+            If True calculates Absolute dispersion. Default True
+        RD: boolean
+            If True calculates relative dispersion. Default True
 
     Returns
     -------
-        GDK_{id}.p (pickle file)
-            Pickle file containing a dictionary with an item per location that includes the x and y values for the Gaussian Dispersal Kernel
+        AD_{id}.p (pickle file)
+            Pickle file containing a dictionary with an item per location that includes the time step, total absolute dispersion, absolute dispersion in x and y components and the number of particles that were available for the calculation
+        RD_{id}.p (pickle file)
+            Pickle file containing a dictionary with an item per location that includes the time step, total relative dispersion, relative dispersion in x and y components and the number of pair of partickes that were available for the calculation
+                        
     """
 
     def __init__(
@@ -69,6 +70,7 @@ class get_dispersion_statistics(object):
         lon = lon.where(lon > 0, lon + 360)
         lat = ds.lat.where(ds.lat < 9.0e35, np.nan)
         self.get_delta_time(ds.time)
+        self.time_step = np.hstack((0, ds['time'].diff('time').dt.total_seconds().cumsum().data))
         status = ds.status.where(~np.isnan(lon), np.nan)
         origin_marker = ds.origin_marker.where(~np.isnan(lon), np.nan)
         age = ds.age_seconds.where(~np.isnan(lon), np.nan)
@@ -92,9 +94,10 @@ class get_dispersion_statistics(object):
         RX = make_nan_array(len(self.release_locations), len(lon.time))
         RY = make_nan_array(len(self.release_locations), len(lon.time))
         RT = make_nan_array(len(self.release_locations), len(lon.time))
+        PP = make_nan_array(len(self.release_locations), len(lon.time))
         for location in self.release_locations:
             print(
-                f"Calculating relative dispersion for location {int(location+1)}/{len(self.release_locations)}"
+                f"--- Calculating relative dispersion for release location {int(location+1)}/{len(self.release_locations)}"
             )
             lon_disp = lon.where(origin_marker == location, drop=True)
             lat_disp = lat.where(origin_marker == location, drop=True)
@@ -103,9 +106,10 @@ class get_dispersion_statistics(object):
             RX_per_location = make_nan_array(len(identify_clusters), len(lon_disp.time))
             RY_per_location = make_nan_array(len(identify_clusters), len(lon_disp.time))
             RT_per_location = make_nan_array(len(identify_clusters), len(lon_disp.time))
+            PP_per_location = make_nan_array(len(identify_clusters), len(lon_disp.time))
             for cluster_count, cluster in enumerate(identify_clusters):
                 print(
-                    f"Location {int(location+1)}: Cluster {cluster_count+1}/{len(identify_clusters)}"
+                    f"--- Location {int(location+1)}: Cluster {cluster_count+1}/{len(identify_clusters)}"
                 )
                 lon_cluster = lon_disp.isel(
                     trajectory=np.arange(cluster_index, cluster + 1, 1)
@@ -117,6 +121,7 @@ class get_dispersion_statistics(object):
                 for time_count, time in enumerate(time_cluster):
                     lon_time_step = lon_cluster.sel(time=time).dropna("trajectory")
                     lat_time_step = lat_cluster.sel(time=time).dropna("trajectory")
+                    pair_of_particles = 1
                     if len(lon_time_step) > 1:
                         for first_count, (lon_trajectory, lat_trajectory) in enumerate(
                             zip(lon_time_step, lat_time_step), 1
@@ -136,6 +141,7 @@ class get_dispersion_statistics(object):
                                 y = haversine(
                                     lon_trajectory, lat_trajectory, lon_trajectory, lat_
                                 )
+                                pair_of_particles+=1
                                 if time == time_cluster[0]:
                                     xo = np.copy(x)
                                     yo = np.copy(y)
@@ -162,6 +168,7 @@ class get_dispersion_statistics(object):
                         RT_per_cluster = np.nanmean(Rx * Ry)
                         RX_per_cluster = np.nanmean(Rx**2)
                         RY_per_cluster = np.nanmean(Ry**2)
+                        pp = pair_of_particles
                     else:
                         RT_per_cluster = np.hstack(
                             (RT_per_cluster, np.nanmean(Rx * Ry))
@@ -172,17 +179,23 @@ class get_dispersion_statistics(object):
                         RY_per_cluster = np.hstack(
                             (RY_per_cluster, np.nanmean(Ry**2))
                         )
+                        pp = np.hstack((pp, pair_of_particles))
+                
                 RT_per_location[cluster_count, 0 : time_count + 1] = RT_per_cluster
                 RX_per_location[cluster_count, 0 : time_count + 1] = RX_per_cluster
                 RY_per_location[cluster_count, 0 : time_count + 1] = RY_per_cluster
+                PP_per_location[cluster_count, 0 : time_count + 1] = pp
                 cluster_index = cluster + 1
             rt = np.nanmean(RT_per_location, axis=0)
             rx = np.nanmean(RX_per_location, axis=0)
             ry = np.nanmean(RY_per_location, axis=0)
+            pair_particles = np.nansum(PP_per_location, axis=0)
             RT[int(location), 0 : len(rt)] = rt
             RX[int(location), 0 : len(rx)] = rx
             RY[int(location), 0 : len(ry)] = ry
-        return RX, RY, RT
+            PP[int(location), 0 : len(pair_particles)] = pair_particles
+            
+        return RX, RY, RT, PP
 
     def get_absolute_dispersion(
         self,
@@ -190,12 +203,14 @@ class get_dispersion_statistics(object):
         lat,
         origin_marker,
     ):
+        """Obtains the absolute dispersion of the dataset"""
         AX = make_nan_array(len(self.release_locations), len(lon.time))
         AY = make_nan_array(len(self.release_locations), len(lon.time))
         AT = make_nan_array(len(self.release_locations), len(lon.time))
+        NP = make_nan_array(len(self.release_locations), len(lon.time))
         for location_count, location in enumerate(self.release_locations):
             print(
-                f"Calculating absolute dispersion for location {int(location+1)}/{len(self.release_locations)}"
+                f"--- Calculating absolute dispersion for release location {int(location+1)}/{len(self.release_locations)}"
             )
             lon_disp = lon.where(origin_marker == location, drop=True)
             lat_disp = lat.where(origin_marker == location, drop=True)
@@ -216,21 +231,44 @@ class get_dispersion_statistics(object):
                         lon_trajectory = lon_.isel(time=time_count)
                         lat_trajectory = lat_.isel(time=time_count)
                         x = haversine(lon0, lat0, lon_trajectory, lat0)
-                        y = haversine(lat0, lat0, lon0, lat_trajectory)
+                        y = haversine(lon0, lat0, lon0, lat_trajectory)
                         X = np.hstack((X, x))
                         Y = np.hstack((Y, y))
                 DX_per_location[count, 0 : time_count + 1] = X
                 DY_per_location[count, 0 : time_count + 1] = Y
             AX[location_count, :] = np.nanmean(DX_per_location**2, axis=0)
             AY[location_count, :] = np.nanmean(DY_per_location**2, axis=0)
-            AT[location_count, :] = AX[location_count, :] + AY[location_count, :]
-        return AX, AY, AT
+            AT[location_count, :] = AX[location_count,:] + AY[location_count,:]
+            NP[location_count, :] = self.get_number_of_particles_analysed(DX_per_location)
+        
+        return AX, AY, AT, NP
+
+    def get_number_of_particles_analysed(self, var):
+        total_time = var.shape[1]
+        valid_particles = make_nan_array(total_time)
+        for count in range(total_time):
+            find_valid_particles = np.where(~np.isnan(var[:,count]))
+            valid_particles[count] = len(find_valid_particles[0])
+        return valid_particles
+        
+    def make_dict(self, vars, vars_names):
+        """Makes dictionary of provided variables by location"""
+        dict_disp={}
+        for count, location in enumerate(self.release_locations):
+            temp_dict= dict()
+            for var, name in zip(vars,vars_names):
+                if 'time' in name:
+                    temp_dict[name] = var
+                else:
+                    temp_dict[name] = var[count,:]
+            dict_disp[f'loc_{"%02d"%location}']=temp_dict
+        return dict_disp
 
     def run(self):
         self.set_directories()
         print(f"--- Generating output directory")
-        for file in self.file_list:
-            print(f"--- Analysing {file}")
+        for count, file in enumerate(self.file_list,1):
+            print(f"--- Analysing {file} file {count}/{len(self.file_list)}")
             ds = xr.open_dataset(file)
             (
                 lon,
@@ -244,7 +282,7 @@ class get_dispersion_statistics(object):
                 ~np.isnan(np.unique(origin_marker))
             ]
             if self.AD:
-                AX, AY, AT = self.get_absolute_dispersion(
+                AX, AY, AT, NP = self.get_absolute_dispersion(
                     lon,
                     lat,
                     origin_marker,
@@ -253,14 +291,16 @@ class get_dispersion_statistics(object):
                     self.AT = np.expand_dims(AT, 0)
                     self.AX = np.expand_dims(AX, 0)
                     self.AY = np.expand_dims(AY, 0)
+                    self.NP = np.expand_dims(NP, 0)
                     self.first_AD = False
                 else:
                     self.AT = np.vstack((self.AT, np.expand_dims(AT, 0)))
                     self.AX = np.vstack((self.AX, np.expand_dims(AX, 0)))
                     self.AY = np.vstack((self.AY, np.expand_dims(AY, 0)))
+                    self.NP = np.vstack((self.NP, np.expand_dims(NP, 0)))
 
             if self.RD:
-                RX, RY, RT = self.get_relative_dispersion(
+                RX, RY, RT, PP = self.get_relative_dispersion(
                     lon,
                     lat,
                     origin_marker,
@@ -269,26 +309,34 @@ class get_dispersion_statistics(object):
                     self.RT = np.expand_dims(RT, 0)
                     self.RX = np.expand_dims(RX, 0)
                     self.RY = np.expand_dims(RY, 0)
+                    self.PP = np.expand_dims(PP, 0)
                     self.first_RD = False
                 else:
                     self.RT = np.vstack((self.RT, np.expand_dims(RT, 0)))
                     self.RX = np.vstack((self.RX, np.expand_dims(RX, 0)))
                     self.RY = np.vstack((self.RY, np.expand_dims(RY, 0)))
-            if self.AD:
-                print(f"--- Saving Absolute Dispersion to {self.outdir}/AD_{self.id}.p")
-                pickle.dump(
-                    np.nanmean(self.AT,0),
-                    np.nanmean(self.AX,0),
-                    np.nanmean(self.AY,0),
-                    open(f"{self.outdir}/AD_{self.id}.p", "wb"),
-                )
+                    self.PP = np.vstack((self.PP, np.expand_dims(PP, 0)))
+        if self.AD:
+            print(f"--- Saving Absolute Dispersion to {self.outdir}/AD_{self.id}.p")
+            AT_avg = np.nanmean(self.AT,0)
+            AX_avg = np.nanmean(self.AX,0)
+            AY_avg = np.nanmean(self.AY,0)
+            NP_accum = np.nansum(self.NP,0)
+            AD_dict = self.make_dict([self.time_step, AT_avg, AX_avg, AY_avg, NP_accum], ['time_step','AD_t','AD_x','AD_y','Number_of_Particles'])
+            pickle.dump(
+                AD_dict,
+                open(f"{self.outdir}/AD_{self.id}.p", "wb"),
+            )
 
-            if self.RD:
-                print(f"--- Saving Relative Dispersion to {self.outdir}/RD_{self.id}.p")
-                pickle.dump(
-                    np.nanmean(self.RT,0),
-                    np.nanmean(self.RX,0),
-                    np.nanmean(self.RY,0),
-                    open(f"{self.outdir}/RD_{self.id}.p", "wb"),
-                )
-            print("--- Done")
+        if self.RD:
+            RT_avg = np.nanmean(self.RT,0)
+            RX_avg = np.nanmean(self.RX,0)
+            RY_avg = np.nanmean(self.RY,0)
+            PP_accum = np.nansum(self.PP,0)                
+            print(f"--- Saving Relative Dispersion to {self.outdir}/RD_{self.id}.p")
+            RD_dict = self.make_dict([self.time_step, RT_avg, RX_avg, RY_avg, PP_accum], ['time_step','RD_t','RD_x','RD_y','Number_of_Particle_Pairs'])
+            pickle.dump(
+                RD_dict,
+                open(f"{self.outdir}/RD_{self.id}.p", "wb"),
+            )
+        print("--- Done")
--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 
 - [Lagrangian\_Analyses](#lagrangian_analyses)
   - [Description](#description)
+  - [Conda Installation](#conda-installation)
+  - [Docker Image](#docker-image)
+  - [Contents](#contents)
+    - [Lagrangian\_Analyses](#lagrangian_analyses-1)
+    - [notebooks](#notebooks)
+      - [data](#data)
+      - [output](#output)
+  - [References](#references)
+  - [Additional info](#additional-info)
+      - [Personalised colourmap options ](#personalised-colourmap-options-)
 
 ## Description
-Repository containing the Lagrangian Analyses used on Montano Orozco, M. M. (2023). Hydrodynamics and coastal dispersion from the Bay of Plenty, Aotearoa, New Zealand: A 25-year numerical modelling perspective (Thesis, Doctor of Philosophy). University of Otago. Retrieved from http://hdl.handle.net/10523/16243. 
+Repository containing the Lagrangian Analyses used on Montaño Orozco, M. M. (2023). Hydrodynamics and coastal dispersion from the Bay of Plenty, Aotearoa, New Zealand: A 25-year numerical modelling perspective (Thesis, Doctor of Philosophy). University of Otago. Retrieved from http://hdl.handle.net/10523/16243. 
 
 [For a better formatted version of the thesis (working embedded links) please click here](https://drive.google.com/file/d/1WMgq2lu7K5MjGTy6O5YpoKDkONDclkHo/view?usp=sharing)
 
@@ -14,9 +24,111 @@ The analyses done for chapter 3 "A climatological baseline of the simulated Eule
 
 Analyses and calculations covered in this repository regarding chapter 2 and chapter 4 include:
 - Absolute and relative dispersion
-- Spaghetti Plots
 - PDF (Probability Density Functions)
 - Connectivity Matrices
 - Dispersal Kernels
 - Stranded Particle data (average duration before stranding and locations)
 
+
+## Conda Installation
+To use this package OpenDrift is necessary. To facilitate and make the installation faster please use [mamba](https://github.com/conda-forge/miniforge). 
+Example of how to install on Unix-like platforms (Mac OS & Linux):
+```
+curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
+
+bash Miniforge3-$(uname)-$(uname -m).sh
+```
+*Accessed: December 2023
+
+Once mamba is installed we can proceed.
+
+This research was part of the [Moana Project](www.moanaproject.org), therefore the reader used for OpenDrift is especific to a reader developed by [Simon Weppe](https://github.com/simonweppe) for the project use. However, unless working with the same dataset or a data related to the Moana project I invite you to download the repository directly from OpenDrift
+
+OpenDrift original repo
+```
+git clone https://github.com/OpenDrift/opendrift.git
+```
+
+Branch developed by Simon Weppe
+```
+git clone https://github.com/simonweppe/opendrift.git
+```
+
+The [environment.yml](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/environment.yml) file contains all the dependencies needed to run opendrift and cLCS scripts.
+```
+cd ../
+git clone https://github.com/MireyaMMO/Lagrangian_Analyses.git 
+cd Lagrangian_Analyses
+mamba env create --name Lagrangian --file=environment.yml
+pip install --no-deps -e .
+cd ../opendrift
+pip install --no-deps -e .
+```
+
+## Docker Image
+To build a local docker image:
+```
+docker build -f Dockerfile -t lagrangian:$tag .
+```
+
+To pull a working docker image from the current repository
+
+Moana Version (developed by Simon Weppe)
+```
+docker pull mmontao/lagrangian:MOANA-v1.0.0   
+```
+OpenDrift
+```
+docker pull mmontao/lagrangian:v1.0.0   
+```
+
+And run. $tag can be v1.0.0, MOANA-v1.0.0 or if built locally the chosen tag
+```
+docker run -ti mmontao/lagrangian:$tag bash
+```
+
+## Contents
+### [Lagrangian_Analyses](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/Lagrangian_Analyses)
+  - [opendrift.py](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/opendrift.py) contains the `opendrift_run` class that deploys particles using OpenDrift.
+  - [get_connectivity_statistics.py](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/get_connectivity_statistics.py) contains the class `get_connectivity_statistics` which uses the OpenDrift output file to calculate the 2D Probability Density Function Connectivity Matrix and coefficient of variance, plus some stranded/settled particles statistics. 
+  - [get_dispersal_kernel.py](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/get_dispersal_kernel.py) contains the class `get_dispersal_kernel` which uses the OpenDrift output file to calculate the dispersal kernel for each location. 
+  - [get_dispersion_statistics.py](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/get_dispersion_statistics.py) contains the class `get_dispersion_statistics` which uses the OpenDrift output file to calculate the relative and absolute dispersion for each location.
+  - [utils.py](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/utils.py) contains some utility functions used accross the different classes such as `haversine`, `sp2xy`,  `xy2sph` and `get_colourmap`. The latter contains [personalised colourmap options ](#personalised-colourmap-options-) for a different variety of colourmaps (see Additional info). 
+### [notebooks](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks)
+  - [01_opendrift_run.ipynb](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/notebooks/01_opendrift_run.ipynb): Jupyter Notebook containing an example of how to use [opendrift_run](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/opendrift.py)
+  - [02_connectivity_statistics.ipynb](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/notebooks/02_lagrangian_statistics.ipynb): Jupyter Notebook containing an example of how to use [get_connectivity_statistics](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/get_connectivity_statistics.py).
+  - [03_dispersal_kernel.ipynb](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/notebooks/03_dispersal_kernel.ipynb): Jupyter Notebook containing an example of how to use [get_dispersal_kernel](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/get_dispersal_kernel.py).
+  - [04_dispersion_statistics.ipynb](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/notebooks/04_dispersion_statistics.ipynb): Jupyter Notebook containing an example of how to use [get_dispersion_statistics](https://github.com/MireyaMMO/Lagrangian_Analyses/blob/main/Lagrangian_Analyses/get_dispersion_statistics.py).
+#### data
+- `%yyyy%mm_Particles_40_nb.nc`
+Contains the data needed to replicate the PDF, Connectivity Matrix and coefficient of variance for the January experiment as seen in Chapter 4 of Montano et al 2023. Map of regions shown below. However to reduce the size of the repository only the years from 2010 to 2020 are added. 
+
+#### output
+Contains the output obtained from each of the jupyter notebooks mentioned above 
+  - [notebook_01](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_01)
+    - [200301_Particles_control.nc](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_01/200301_Particles_control.nc) OpenDrift output
+    - [200401_Particles_control.nc](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_01/200401_Particles_control.nc)
+  - [notebook_02](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_02)
+    -  [CM_control.p](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_02/CM_control.p) Connectivity Matrix pickle file
+    -  [PDF_control.p](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_02/PDF_control.p) PDF pickle file
+    -  [Stranded_particles_data_control.p](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_02/Stranded_particles_data_control.p) Pickle file containing information regarding the particles stranded. 
+  - [notebook_03](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_03)
+    - [GDK_control.p](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_03/GDK_control.p) Pickle file containing a dictionary with the Gaussian Dispersal Kernel information for each location.
+  - [notebook_04](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_04)
+    - [AD_control.p](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_04/AD_control.p) Pickle file containing a dictionary with the Absolute Dispersion information for each location.
+    - [RD_control.p](https://github.com/MireyaMMO/Lagrangian_Analyses/tree/main/notebooks/output/notebook_04/RD_control.p) Pickle file containing a dictionary with the Relative Dispersion information for each location.
+
+## References
+**Moana Project - BoP Study**
+- [Work in progress] Montaño M. M., Suanda, S. H., de Souza, J. M. A. C., Duran R. "A climatological baseline of the quasi-steady Lagrangian transport patterns in a large coastal embayment."
+- [Work in progress] Montaño M. M., Suanda, S. H., de Souza, J. M. A. C. "Hydrodynamic and behaviour parametrization effects on modelled coastal connectivity."
+- Montaño, M. M., Suanda, S. H., and Souza, J. M. A. C. d. (2023). Modelled coastal circulation and Lagrangian statistics from a large coastal embayment: The case of Bay of Plenty, Aotearoa New Zealand. Estuarine, Coastal and Shelf Science, 281, [10.1016/j.ecss.2023.108212](https://doi.org/10.1016/j.ecss.2023.108212). 
+- Montaño Orozco, M. M. (2023). Hydrodynamics and coastal dispersion from the Bay of Plenty, Aotearoa, New Zealand: A 25-year numerical modelling perspective (Thesis, Doctor of Philosophy). University of Otago. Retrieved from http://hdl.handle.net/10523/16243. 
+
+[For a better formatted version of the thesis (working embedded links) please click here](https://drive.google.com/file/d/1WMgq2lu7K5MjGTy6O5YpoKDkONDclkHo/view?usp=sharing)
+
+- Moana Project Data on [THREDDS](http://thredds.moanaproject.org:6443/thredds/catalog/moana/catalog.html) 
+
+
+## Additional info
+#### Personalised colourmap options ![Alt text](notebooks/data/Colourmap_examples.png)
